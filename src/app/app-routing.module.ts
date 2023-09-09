cbe96c3e567ae716c9c789d8c5c8ec0ff5de7484
esky <eugene.sky.dev@gmail.com>
2023-09-09 23:23:05 +0300

    login starter: lazy loaded module & component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'truck-grid', loadChildren: () => import('./feature-truck-grid/feature-truck-grid.module').then(m => m.FeatureTruckGridModule) },
   { path: '', loadChildren: () => import('./feature-truck-grid/feature-truck-grid.module').then(m => m.FeatureTruckGridModule) }
 ]
