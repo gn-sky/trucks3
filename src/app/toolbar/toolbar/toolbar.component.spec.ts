527432bbecd3f47542ab15290b6e3566a7fd1519
Eugene Sky <eugene.sky.dev@gmail.com>
2023-09-19 00:40:05 +0300

    MockAuthService (#12)

--- src/app/toolbar/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar/toolbar.component.spec.ts
@@ -3,21 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToolbarComponent } from './toolbar.component';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from './mock-auth.service';
+
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: AngularFireAuth, useValue: {} }],
-      declarations: [ToolbarComponent]
+      declarations: [ToolbarComponent],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService }
+      ]
     });
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', () => { 
     expect(component).toBeTruthy();
   });
 });
